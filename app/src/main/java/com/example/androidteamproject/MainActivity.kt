7ad8f59e4b9c38336b44adb62b751ec04b4334cf
yujun <yoojun20@gmail.com>
2022-11-09 22:59:08 +0900

    Init Main_XML file setting
    MainActivity.kt, activity_main.xml 을 앱에 필요한 최소한의 기능을 동작하기 위한 것을 설정

--- app/src/main/java/com/example/androidteamproject/MainActivity.kt
+++ app/src/main/java/com/example/androidteamproject/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.example.androidteamproject
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Switch
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var swAppEnabled : Switch
+    lateinit var tvDisabled : TextView
+    lateinit var tvEnabled : TextView
+    lateinit var btnAMode : Button
+    lateinit var btnBMode : Button
+    lateinit var btnSetting : Button
+    fun swAppCheck(b:Boolean){
+        //앱 활성화, 비활성화 체크 스위치 동작 -> 활성화, 비활성화 텍스트 설정
+        if(b){
+            tvDisabled.setTextColor(Color.WHITE)
+            tvEnabled.setTextColor(Color.BLACK)
+        }
+        else{
+            tvDisabled.setTextColor(Color.BLACK)
+            tvEnabled.setTextColor(Color.WHITE)
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        swAppEnabled = findViewById(R.id.swAppEnabled)
+        tvDisabled = findViewById(R.id.tvDisabled)
+        tvEnabled = findViewById(R.id.tvEnabled)
+        btnAMode = findViewById(R.id.btnAMode)
+        btnBMode = findViewById(R.id.btnBMode)
+        btnSetting = findViewById(R.id.btnSetting)
+
+        swAppCheck(swAppEnabled.isChecked)
+
+        swAppEnabled.setOnCheckedChangeListener { compoundButton, b ->
+            swAppCheck(b)//활성화, 비활성화 텍스트 설정
+            if(b){
+                //앱 활성화 시 동작
+            }
+            else{
+                //앱 비활성화 시 동작
+            }
+        }
+
+        btnAMode.setOnClickListener {
+            //A 모드 버튼 클릭 시 동작
+        }
+        btnBMode.setOnClickListener {
+            //B 모드 버튼 클릭 시 동작
+        }
+        btnSetting.setOnClickListener {
+            //설정 버튼 클릭 시 동작
+        }
     }
 }
