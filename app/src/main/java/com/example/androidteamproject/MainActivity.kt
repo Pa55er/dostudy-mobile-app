6c4d49d368a60dfa170be0bc5ce2f35175fb0251
김종훈 <1924130@donga.ac.kr>
2022-11-20 22:34:33 +0900

    Feat: 알림 버그 수정 및 DB 선언 및 연동
    알림이 뜨지 않는 버그 수정
    A Mode, B Mode에 대해 패키지들의 상태를 저장할 DB 추가,
    앱 시작시 현재 설치된 패키지들과 DB에 있는 패키지들 동기화하는 코드 추가
    
    TODO:
    UI 다듬기
    각 모드에서 DB에 저장된 패키지들 가져와서 출력하는 코드 추가
    각 모드에서 각 패키지에 대해 설정하는 코드 추가
    오버레이를 사용하여 앱 차단 기능 추가

--- app/src/main/java/com/example/androidteamproject/MainActivity.kt
+++ app/src/main/java/com/example/androidteamproject/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.example.androidteamproject
 
+import android.app.NotificationManager
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,11 +19,24 @@ class MainActivity : AppCompatActivity() {
     lateinit var btnSetting : Button
     lateinit var btnD : Button
     lateinit var btnW : Button
+    lateinit var aModeDB: AModeDatabase
+    lateinit var bModeDB: BModeDatabase
 
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val notificationManager =
+            getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(
+            NotificationChannelManager.StudyNotificationChannel.notificationChannel
+        )
+
+        aModeDB = AModeDatabase.getInstance(applicationContext)!!
+        bModeDB = BModeDatabase.getInstance(applicationContext)!!
+        databaseSynchronization()
 
         btnAMode = findViewById<Button>(R.id.btnAMode)
         btnBMode = findViewById<Button>(R.id.btnBMode)
@@ -53,5 +73,55 @@ class MainActivity : AppCompatActivity() {
             overridePendingTransition(0, 0)//인텐트 효과 없애기
         }
 
+    }
+
+    private fun databaseSynchronization() {
+        // 설치된 패키지들을 가져오기 위한 intent
+        var intent = Intent(Intent.ACTION_MAIN, null)
+        intent.addCategory(Intent.CATEGORY_LAUNCHER)
+
+        // 현재 설치된 패키지들
+        var pkgList: MutableMap<String, String> =
+            applicationContext
+                .packageManager
+                .queryIntentActivities(intent, 0).associate {
+                    it.activityInfo.packageName to it.activityInfo.loadLabel(packageManager)
+                        .toString()
+                }.toMutableMap()
+
+        // DB에 저장된 패키지들
+        var pkgListDB = runBlocking {
+            withContext(CoroutineScope(Dispatchers.Default).coroutineContext) {
+                aModeDB.aModePackageDataDao().getAll()
+            }
+        }.map { it.packageName }
+
+        // DB에 저장된 패키지들을 pkgList에서 제거하여 추가된 패키지 들만 남긴다
+        // 만약 pkgList에 없다면 삭제된 패키지이므로 DB에서 제거한다
+        pkgListDB.forEach {
+            if (pkgList.contains(it)) pkgList.remove(it)
+            else {
+                runBlocking {
+                    withContext(CoroutineScope(Dispatchers.Default).coroutineContext) {
+                        aModeDB.aModePackageDataDao().deleteDataByPackageName(it)
+                        bModeDB.bModePackageDataDao().deleteDataByPackageName(it)
+                    }
+                }
+            }
+        }
+
+        // 새로 추가된 패키지들을 DB에 추가한다
+        pkgList.forEach {
+            runBlocking {
+                withContext(CoroutineScope(Dispatchers.Default).coroutineContext) {
+                    aModeDB.aModePackageDataDao().insert(
+                        AModePackageData(it.key, it.value, false)
+                    )
+                    bModeDB.bModePackageDataDao().insert(
+                        BModePackageData(it.key, it.value, false, 0)
+                    )
+                }
+            }
+        }
     }
 }
