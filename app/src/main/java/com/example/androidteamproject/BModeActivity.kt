6a2a312a89d9966148284c2f5f6c63c2a4e502aa
yujun <yoojun20@gmail.com>
2022-11-28 00:16:23 +0900

    Add overlay.jpg Change overlay view BModeActivity.kt test checkBox

--- app/src/main/java/com/example/androidteamproject/BModeActivity.kt
+++ app/src/main/java/com/example/androidteamproject/BModeActivity.kt
@@ -1,6 +1,10 @@
 package com.example.androidteamproject
 
-<<<<<<< HEAD
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+
 import android.app.ActionBar.LayoutParams
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -15,6 +19,7 @@ import android.widget.RelativeLayout
 import android.widget.TextView
 import android.widget.TimePicker
 import android.widget.Toast
+import androidx.core.view.GravityCompat
 import androidx.core.view.isVisible
 import org.w3c.dom.Text
 
@@ -36,58 +41,21 @@ class BModeActivity : AppCompatActivity() {
     lateinit var npH : NumberPicker
     lateinit var npM : NumberPicker
     lateinit var npS : NumberPicker
-=======
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.view.View
-import android.widget.*
-import androidx.core.view.isVisible
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
-
-class BModeActivity : AppCompatActivity() {
-    lateinit var tvBModeSettingTime : TextView // 현재 상태 표시용 텍스트 뷰
-    lateinit var LLBModeAppList : LinearLayout // 앱 리스트 동적 추가 할 장소
-    lateinit var btnBModeSave : Button // 저장 버튼
     lateinit var bModeDB: BModeDatabase
 
-    lateinit var tvAppState : TextView
-    lateinit var btnTimeSetting : Button
+    lateinit var tvBModeSettingTime : TextView // 현재 상태 표시용 텍스트 뷰
+
     lateinit var tpAppTimeSetting : TimePicker
->>>>>>> ab3769fe7c00dd7f937b8464e0923dc69fdbf1bb
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bmode)
 
-<<<<<<< HEAD
-=======
         bModeDB = BModeDatabase.getInstance(applicationContext)!!
 
-        tvBModeSettingTime = findViewById(R.id.tvBModeSettingTime)
->>>>>>> ab3769fe7c00dd7f937b8464e0923dc69fdbf1bb
         LLBModeAppList = findViewById(R.id.LLBModeAppList)
         btnBModeSave = findViewById(R.id.btnBModeSave)
 
-
-<<<<<<< HEAD
-
-        // 앱 리스트 가져오기 필요
-        var strApp = arrayListOf<String>("app1","app2","app3","app4","app5","app6","app7","app8","app9","app10")
-        for(i in 0..strApp.size-1){
-            llAppNameCheckbox = LinearLayout(this)
-            //llAppNameCheckbox.orientation = LinearLayout.HORIZONTAL
-            var ll = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT)
-            llAppNameCheckbox.layoutParams = ll
-
-            chbxApp = CheckBox(this)//앱 활성화 체크 박스
-            chbxApp.id = i+20000
-
-            tvAppState = TextView(this) //앱 상태 출력 텍스트 뷰
-            tvAppState.text = strApp[i]
-=======
         var pkgListDB = runBlocking {
             withContext(CoroutineScope(Dispatchers.Default).coroutineContext) {
                 bModeDB.bModePackageDataDao().getAll()
@@ -98,20 +66,19 @@ class BModeActivity : AppCompatActivity() {
 
         // 앱 리스트 가져오기 필요
         var strApp = pkgListDB.map { it.appName }
+        //var strApp = arrayListOf("app111111111","app2")
         for(i in 0..strApp.size-1){
             tvAppState = TextView(this) //앱 상태 출력 텍스트 뷰
             tvAppState.text = strApp[i]
             // bias 10000 -> 앱 이름 TextView
             // bias 100000 -> 시간세팅 버튼
             // bias 20000 -> Checkbox
->>>>>>> ab3769fe7c00dd7f937b8464e0923dc69fdbf1bb
             tvAppState.id = i+10000
 
             btnTimeSetting = Button(this) // 앱 시간 세부 설정 버튼
             btnTimeSetting.text = "시간 세부 설정"
             btnTimeSetting.id = i
 
-<<<<<<< HEAD
             //Number Picker Setting
             llSettingNP = LinearLayout(this)
             var llNP = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT)
@@ -166,6 +133,13 @@ class BModeActivity : AppCompatActivity() {
             npS.id = i+50000
 
             //앱 이름과 체크박스 추가
+            llAppNameCheckbox = LinearLayout(this)
+            llAppNameCheckbox.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT)
+            llAppNameCheckbox.orientation = LinearLayout.HORIZONTAL
+            var tmpView = View(this)
+            chbxApp = CheckBox(this)
+            chbxApp.id = i+200000
+            chbxApp.gravity = GravityCompat.END
             llAppNameCheckbox.addView(tvAppState)
             llAppNameCheckbox.addView(chbxApp)
 
@@ -186,7 +160,6 @@ class BModeActivity : AppCompatActivity() {
             LLBModeAppList.addView(llAppNameCheckbox)
             LLBModeAppList.addView(btnTimeSetting)
             LLBModeAppList.addView(llSettingNP)
-=======
             tpAppTimeSetting = TimePicker(this) // 앱 시간 설정 타임피커
             tpAppTimeSetting.visibility = View.GONE
             tpAppTimeSetting.id = i+100000
@@ -201,17 +174,17 @@ class BModeActivity : AppCompatActivity() {
 //            //nujkjjhfjfjfj
 //            if(pkgListDB[i].isChecked) ~~~~.isChecked = true
 //            else ~~~.isChecked = false
->>>>>>> ab3769fe7c00dd7f937b8464e0923dc69fdbf1bb
         }
 
         for(i in 0..strApp.size-1) {
-            findViewById<Button>(i).setOnClickListener { // 시간 세부 설정 버튼 클릭 시 동작
-<<<<<<< HEAD
-                // 앱 별로 넘버 피커 화면 출력 여부
-                if (findViewById<LinearLayout>(i+200000).isVisible)
-                    findViewById<LinearLayout>(i+200000).visibility = View.GONE
-                else findViewById<LinearLayout>(i+200000).visibility = View.VISIBLE
-            }
+            try{
+                findViewById<Button>(i).setOnClickListener { // 시간 세부 설정 버튼 클릭 시 동작
+                    // 앱 별로 넘버 피커 화면 출력 여부
+                    if (findViewById<LinearLayout>(i+200000).isVisible)
+                        findViewById<LinearLayout>(i+200000).visibility = View.GONE
+                    else findViewById<LinearLayout>(i+200000).visibility = View.VISIBLE
+                }
+            }catch (e:Exception){}
         }
         btnBModeSave.setOnClickListener {
             // 저장 버튼 클릭 시 동작
@@ -221,30 +194,27 @@ class BModeActivity : AppCompatActivity() {
             * 분 : findViewById<NumberPicker>(index+40000)
             * 초 : findViewById<NumberPicker>(index+50000)
             * */
-=======
-                // 앱 별로 타임 피커 화면 출력 여부
-                if (findViewById<TimePicker>(i+100000).isVisible)
-                    findViewById<TimePicker>(i+100000).visibility = View.GONE
-                else findViewById<TimePicker>(i+100000).visibility = View.VISIBLE
-            }
-
-        }
-        btnBModeSave.setOnClickListener {
             // 저장 버튼 클릭 시 동작
-            for(i in 0..strApp.size-1) {
-                if(findViewById<CheckBox>(i + 200000).isChecked) {
-                    var target = findViewById<NumberPicker>(i+100000)
-                    pkgListDB[i].isChecked = true
-                    // 수정 필요
-//                    pkgListDB[i].limitHour = tpAppTimeSetting.hour
-//                    pkgListDB[i].limitMinute = tpAppTimeSetting.minute
+            try{
+                for(i in 0..strApp.size-1) {
+                    if(findViewById<CheckBox>(i + 200000).isChecked) {
+                        var target = findViewById<NumberPicker>(i+100000)
+                        pkgListDB[i].isChecked = true
+
+                        // 앱 별로 타임 피커 화면 출력 여부
+                        if (findViewById<TimePicker>(i+100000).isVisible)
+                            findViewById<TimePicker>(i+100000).visibility = View.GONE
+                        else findViewById<TimePicker>(i+100000).visibility = View.VISIBLE
+                        // 수정 필요
+    //                    pkgListDB[i].limitHour = tpAppTimeSetting.hour
+    //                    pkgListDB[i].limitMinute = tpAppTimeSetting.minute
+                    }
+                    else {
+                        pkgListDB[i].isChecked = false
+                    }
                 }
-                else {
-                    pkgListDB[i].isChecked = false
-                }
-            }
-            finish()
->>>>>>> ab3769fe7c00dd7f937b8464e0923dc69fdbf1bb
+                finish()
+            }catch (e:Exception){}
         }
     }
 }
